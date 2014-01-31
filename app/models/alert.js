ca2207f26059cbb8b3fa54c42abeda20aba3b643
Pavel Bastov <pbastov@gmail.com>
2014-01-31 17:14:24 +0600

    Add different labels for late days

--- app/models/alert.js
+++ app/models/alert.js
@@ -1,6 +1,26 @@
 OpenCBS.Alert = OpenCBS.Model.extend({
   contractCode: null,
-  lateDays: 0
+  lateDays: 0,
+
+  lateDaysClass: function() {
+    var lateDays = this.get('lateDays');
+
+    if (lateDays > 365) {
+      return 'par-365';
+    } else if (lateDays > 180 && lateDays <= 365) {
+      return 'par-181-365';
+    } else if (lateDays > 90 && lateDays <= 180) {
+      return 'par-91-180';
+    } else if (lateDays > 60 && lateDays <= 90) {
+      return 'par-61-90';
+    } else if (lateDays > 30 && lateDays <= 60) {
+      return 'par-31-60';
+    } else if (lateDays > 1 && lateDays <= 30) {
+      return 'par-1-30';
+    } else {
+      return 'par-0';
+    }
+  }.property('lateDays')
 });
 
 OpenCBS.Alert.reopenClass({
